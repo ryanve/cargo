b048b00a9436e3881d2425558fa27185b6256d7f
Ryan Van Etten <ryanve@gmail.com>
2013-02-26 16:33:17 -0500

    version 0.1

--- cargo.js
+++ cargo.js
@@ -3,7 +3,7 @@
  * @author      Ryan Van Etten <@ryanve>
  * @link        github.com/ryanve/cargo
  * @license     MIT
- * @version     0.x
+ * @version     0.1.0
  */
 
 /*jslint browser: true, devel: true, node: true, passfail: false, bitwise: true, continue: true
@@ -11,14 +11,80 @@
 , regexp: true, undef: true, sloppy: true, stupid: true, sub: true, vars: true, white: true
 , indent: 4, maxerr: 180 */
 
-(function (root, name, factory) {// github.com/umdjs/umd
+(function(root, name, factory) {// github.com/umdjs/umd
     if (typeof module != 'undefined' && module['exports']) { 
         module['exports'] = factory(); // node / ender / common
     } else { root[name] = factory(); } // browser
-}(this, 'cargo', function () {
+}(this, 'cargo', function() {
 
     // Use array notation on public props to ensure safe compilation
     // developers.google.com/closure/compiler/docs/api-tutorial3
     // developers.google.com/closure/compiler/docs/js-for-compiler
+    
+    return (function(window) {
+        
+        var localStorage = window['localStorage']
+          , sessionStorage = window['sessionStorage']
+          , GET = 'getItem'
+          , SET = 'setItem'
+          , REM = 'removeItem'
+          , testStorage = function(api, key) {
+                try {
+                    key = key || '_cargo';
+                    api[SET](key, key);
+                    api[REM](key);
+                    return true;
+                } catch (e) { return false; }
+            }
+          , hasSession = testStorage(sessionStorage)
+          , hasLocal = testStorage(localStorage)
+          , cargo = {};
+
+        function getSessionItem(k) {
+            return sessionStorage[GET](k);
+        }
+
+        function setSessionItem(k, v) {
+            while (typeof v == 'function') {
+                v = v.call(this, sessionStorage[GET](k));
+            }
+            sessionStorage[SET](k, v);
+            return v;
+        }; 
+        
+        function removeSessionItem(k) {
+            return sessionStorage[REM](k);
+        }
+        
+        function getSessionItem(k) {
+            return sessionStorage[GET](k);
+        }
+        
+        function getLocalItem(k) {
+            return localStorage[GET](k);
+        }
+
+        function setLocalItem(k, v) {
+            while (typeof v == 'function') {
+                v = v.call(this, localStorage[GET](k));
+            }
+            localStorage[SET](k, v);
+            return v;
+        }; 
+        
+        function removeLocalItem(k) {
+            return localStorage[REM](k);
+        }
+        
+        cargo['removeLocalItem'] = removeSessionItem;
+        cargo['setLocalItem'] = setLocalItem;
+        cargo['getLocalItem'] = getLocalItem;
+        cargo['removeSessionItem'] = removeSessionItem;
+        cargo['setSessionItem'] = setSessionItem;
+        cargo['getSessionItem'] = getSessionItem;
+        
+        return cargo;
+        
+    }(window));
 
 }));
